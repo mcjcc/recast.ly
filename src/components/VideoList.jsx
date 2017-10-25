365910aa972646c7f7eedfdcb8cba6a5b5d440ff
mcjcc <mcjohnnychen@gmail.com>
2017-10-25 16:20:12 -0700

    Create App, VideoList, VideoListEntry files

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,12 +1,18 @@
-var VideoList = () => (
-  <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-  </div>
-);
+// inputs
+// an array of video entries
+// loop through array to render video entries 
+var VideoList = (props) => {
+  return (
+    <div className="video-list">
+      {props.videos.map(video => 
+        <VideoListEntry video={video} />
+      )}
+    </div>
+  );
+};
+
+
+
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
@@ -17,3 +23,5 @@ VideoList.propTypes = {
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
 window.VideoList = VideoList;
+
+
